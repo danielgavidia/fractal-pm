e75a157319a1ed59f1b6d681cf93ded9bbc01e14
Daniel Gavidia <dgavidia1@gmail.com>
2024-11-04 17:16:26 -0500

    Added value to color util and first iteration of color editor

--- frontend/src/components/ColorEditor.tsx
+++ frontend/src/components/ColorEditor.tsx
@@ -1,20 +1,51 @@
 "use client";
 
+import { valueToColor } from "@/utils/valueToColor";
 import { useState } from "react";
 
 const ColorEditor = () => {
-  const [color, setColor] = useState<number>(0);
+  const [hue, setHue] = useState<number>(180);
+  const [saturation, setSaturation] = useState<number>(50);
+  const [lightness, setLightness] = useState<number>(50);
+  const color = valueToColor(hue, saturation, lightness);
+
   return (
-    <input
-      type="range"
-      min="0"
-      max="360"
-      value={color}
-      onChange={(e) => setColor(Number(e.target.value))}
-      // style={{
-      //   background: rgb,
-      // }}
-    />
+    <div className="flex flex-col space-y-2">
+      {/* Hue */}
+      <input
+        type="range"
+        min="0"
+        max="360"
+        value={hue}
+        onChange={(e) => setHue(Number(e.target.value))}
+        style={{
+          background: color,
+        }}
+      />
+      {/* Saturation */}
+      <input
+        type="range"
+        min="0"
+        max="100"
+        value={saturation}
+        onChange={(e) => setSaturation(Number(e.target.value))}
+        style={{
+          background: color,
+        }}
+      />
+      {/* Lightness */}
+      <input
+        type="range"
+        min="0"
+        max="100"
+        value={lightness}
+        onChange={(e) => setLightness(Number(e.target.value))}
+        style={{
+          background: color,
+        }}
+      />
+      <div className="p-4" style={{ backgroundColor: color }}></div>
+    </div>
   );
 };
 
