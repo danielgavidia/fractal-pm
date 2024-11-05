9d820db083150aa8289cb22934ef6c5108ff62b8
Daniel Gavidia <dgavidia1@gmail.com>
2024-11-05 12:25:21 -0500

    Added epics store

---
+++ frontend/src/stores/epicStore.ts
@@ -0,0 +1,29 @@
+import { Epic } from "@/types/types";
+import { create } from "zustand";
+import { dummyEpics } from "@/api/dummyEpics";
+
+interface EpicStoreState {
+  epics: Epic[];
+  createEpic: (epic: Epic) => void;
+  // createTask: (task: Task) => void;
+  // deleteTask: (taskId: string) => void;
+  // updateTask: (taskId: string, newTask: Task) => void;
+  // updateTaskStatus: (taskId: string, newTaskStatus: TicketStatus) => void;
+}
+
+export const epicStore = create<EpicStoreState>((set) => ({
+  epics: dummyEpics,
+  createEpic: (epic: Epic) => set((state) => ({ epics: [...state.epics, epic] })),
+  // deleteTask: (taskId: string) =>
+  //   set((state) => ({ tasks: state.tasks.filter((task) => task.id !== taskId) })),
+  // updateTask: (taskId: string, newTask: Task) =>
+  //   set((state) => ({
+  //     tasks: state.tasks.map((task) => (task.id === taskId ? newTask : task)),
+  //   })),
+  // updateTaskStatus: (taskId: string, newTaskStatus: TicketStatus) =>
+  //   set((state) => ({
+  //     tasks: state.tasks.map((task) =>
+  //       task.id === taskId ? { ...task, status: newTaskStatus } : task
+  //     ),
+  // })),
+}));
