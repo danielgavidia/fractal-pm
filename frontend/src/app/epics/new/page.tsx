347e2ea0dc3e10468348fae1319592f8d81e99b7
Daniel Gavidia <dgavidia1@gmail.com>
2024-11-05 15:20:17 -0500

    Added logic for redirecting users to new epic page

---
+++ frontend/src/app/epics/new/page.tsx
@@ -0,0 +1,12 @@
+import SectionHeader from "@/components/SectionHeader";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="px-4">
+      <SectionHeader title="New Epic" />
+    </div>
+  );
+};
+
+export default Page;
