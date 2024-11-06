f2cd31d047b25c2b5188c4fdfe3bf69e143538a1
Daniel Gavidia <dgavidia1@gmail.com>
2024-11-06 16:38:51 -0500

    Renamed TicketTabGroup

---
+++ frontend/src/components/TicketTabGroup.tsx
@@ -0,0 +1,25 @@
+import { Ticket, TicketStatus } from "@/types/types";
+
+// Components
+import TicketTab from "@/components/TicketTab";
+import { statusMapping } from "@/utils/statusMapping";
+
+interface TicketTabGroupProps {
+  status: TicketStatus;
+  tickets: Ticket[];
+}
+
+const TicketTabGroup = ({ status, tickets }: TicketTabGroupProps) => {
+  return (
+    <div className="flex flex-col items-center p-1 min-w-48">
+      <p className="text-[10px] py-1 text-left w-full font-bold">{statusMapping[status].label}</p>
+      <div className="flex space-x-2 w-full">
+        {tickets.map((ticket, key) => (
+          <TicketTab key={key} ticket={ticket} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TicketTabGroup;
