6509a7fc816489ea3322f5e84f6916eb300f2c57
Daniel Gavidia <dgavidia1@gmail.com>
2024-11-07 10:26:06 -0500

    Moved Openai functions to serverless backend

--- frontend/src/lib/openaiClient.ts
+++ frontend/src/lib/openaiClient.ts
@@ -1,6 +1,11 @@
+"use server";
+
 import OpenAI from "openai";
 
-export const openaiClient = new OpenAI({
-  apiKey: process.env.NEXT_PUBLIC_OPENAI_KEY,
-  dangerouslyAllowBrowser: true, // Change
-});
+export const getOpenaiClient = async (): Promise<OpenAI> => {
+  const openaiClient = new OpenAI({
+    apiKey: process.env.NEXT_PUBLIC_OPENAI_KEY,
+    // dangerouslyAllowBrowser: true, // Change
+  });
+  return openaiClient;
+};
