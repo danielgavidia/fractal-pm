2596514216b8562fc179a09e5817d04f4d35f288
Daniel Gavidia <dgavidia1@gmail.com>
2024-11-09 11:32:10 -0500

    Added ticket update component

---
+++ frontend/src/components/TicketUpdate.tsx
@@ -0,0 +1,115 @@
+"use client";
+
+import DatePicker from "@/components/DatePicker";
+import SectionHeader from "@/components/SectionHeader";
+import TicketPriorityPicker from "@/components/TicketPriorityPicker";
+import TicketStatusPicker from "@/components/TicketStatusPicker";
+import { ticketStore } from "@/stores/ticketStore";
+import { TicketFinal, TicketPriority, TicketStatus } from "@/types/types";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+interface TicketUpdateProps {
+  ticket: TicketFinal;
+}
+
+const TicketUpdate = ({ ticket }: TicketUpdateProps) => {
+  // const { updateEpic, deleteEpic } = epicStore();
+  const router = useRouter();
+
+  const { updateTicket, deleteTicket } = ticketStore();
+
+  // Local state
+  const [ticketTitle, setTicketTitle] = useState<string>(ticket.title);
+  const [ticketDescription, setTicketDescription] = useState<string>(
+    ticket ? ticket.description : ""
+  );
+  const [ticketStatus, setTicketStatus] = useState<TicketStatus>(
+    ticket ? ticket.status : "notStarted"
+  );
+  const [ticketDueDate, setTicketDueDate] = useState<Date>(ticket ? ticket.dueDate : new Date());
+  const [ticketPriority, setTicketPriority] = useState<TicketPriority>(
+    ticket ? ticket.priority : "low"
+  );
+
+  if (!ticket) {
+    return;
+  }
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const newTicket: TicketFinal = {
+      ...ticket,
+      title: ticketTitle,
+      description: ticketDescription,
+      status: ticketStatus,
+      dueDate: ticketDueDate,
+      priority: ticketPriority,
+    };
+    if (ticketTitle !== "" && ticketDescription !== "") {
+      updateTicket(ticket.id, newTicket);
+      setTicketTitle("");
+      setTicketDescription("");
+      router.push(`/epics/${ticket.id}`);
+    }
+  };
+
+  return (
+    <div className="flex flex-col space-y-4">
+      <SectionHeader title="Update Epic" />
+      <form className="flex flex-col space-y-2">
+        {/* Epic title */}
+        <p className="text-xs">Title</p>
+        <input
+          type="text"
+          value={ticketTitle}
+          onChange={(e) => setTicketTitle(e.target.value)}
+          placeholder="Title"
+          className="p-2 text-xs outline-none text-black rounded"
+        />
+
+        {/* Epic description */}
+        <p className="text-xs">Description</p>
+        <textarea
+          value={ticketDescription}
+          onChange={(e) => setTicketDescription(e.target.value)}
+          placeholder="Description"
+          className="p-2 text-xs outline-none text-black rounded"
+        ></textarea>
+      </form>
+
+      {/* Due date */}
+      <div className="flex flex-col space-y-2">
+        <p className="text-xs">Due Date</p>
+        <DatePicker onDateChange={(date: Date | undefined) => date && setTicketDueDate(date)} />
+      </div>
+
+      {/* Epic status */}
+      <div>
+        <p className="text-xs">Status</p>
+        <TicketStatusPicker callback={setTicketStatus} defaultStatus={ticketStatus} />
+      </div>
+
+      {/* Priority */}
+      <div>
+        <p className="text-xs">Priority</p>
+        <TicketPriorityPicker callback={setTicketPriority} defaultPriority={ticketPriority} />
+      </div>
+
+      {/* Update */}
+      <button onClick={handleSubmit} className="w-full border-[1px] p-2 rounded">
+        Update
+      </button>
+
+      {/* Delete */}
+      <button
+        onClick={() => deleteTicket(ticket.id)}
+        className="w-full border-[1px] border-red-500 text-red-500 p-2 rounded"
+      >
+        Delete
+      </button>
+    </div>
+  );
+};
+
+export default TicketUpdate;
--- frontend/src/stores/ticketStore.ts
+++ frontend/src/stores/ticketStore.ts
@@ -5,23 +5,20 @@ import { dummyTickets } from "@/api/dummyTickets";
 interface TicketStoreState {
   tickets: TicketFinal[];
   createTicket: (ticket: TicketFinal) => void;
-  // createTaskMultiple: (tasks: Task[]) => void;
-  // deleteTask: (taskId: string) => void;
-  // updateTask: (taskId: string, newTask: Task) => void;
+  deleteTicket: (ticketId: string) => void;
+  updateTicket: (ticketId: string, newTicket: TicketFinal) => void;
   // updateTaskStatus: (taskId: string, newTaskStatus: TicketStatus) => void;
 }
 
 export const ticketStore = create<TicketStoreState>((set) => ({
   tickets: dummyTickets,
   createTicket: (ticket: TicketFinal) => set((state) => ({ tickets: [...state.tickets, ticket] })),
-  // createTask: (task: Task) => set((state) => ({ tasks: [...state.tasks, task] })),
-  // createTaskMultiple: (tasks: Task[]) => set((state) => ({ tasks: [...state.tasks, ...tasks] })),
-  // deleteTask: (taskId: string) =>
-  //   set((state) => ({ tasks: state.tasks.filter((task) => task.id !== taskId) })),
-  // updateTask: (taskId: string, newTask: Task) =>
-  //   set((state) => ({
-  //     tasks: state.tasks.map((task) => (task.id === taskId ? newTask : task)),
-  //   })),
+  deleteTicket: (ticketId: string) =>
+    set((state) => ({ tickets: state.tickets.filter((ticket) => ticket.id !== ticketId) })),
+  updateTicket: (ticketId: string, newTicket: TicketFinal) =>
+    set((state) => ({
+      tickets: state.tickets.map((ticket) => (ticket.id === ticketId ? newTicket : ticket)),
+    })),
   // updateTaskStatus: (taskId: string, newTaskStatus: TicketStatus) =>
   //   set((state) => ({
   //     tasks: state.tasks.map((task) =>
